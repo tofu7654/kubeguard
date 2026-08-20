eb2f6255b08f2e6d050f2b697ece8f6d10f75b2a
Toufeeq <tsharieff@Toufeeqs-MacBook-Pro.local>
2026-08-20 19:44:31 -0400

    add argparse to cli capabilities

--- src/kubeguard/cli.py
+++ src/kubeguard/cli.py
@@ -1,22 +1,34 @@
 from typing import Any
-import sys
-import kubeguard.input as input
+import kubeguard.loader as loader
 import kubeguard.analyzer as analyzer
+import argparse
 
 def main() -> None:
-    # get path of json file from command line arg
-    if len(sys.argv) < 2:
-        print("Usage: python kubeguard.py <pods.json>")
-        sys.exit(1)
+    # use argparse instead of sys.argv
+    parser = argparse.ArgumentParser(
+        prog='KubeGuard',
+        description='Kubernetes health check CLI'
+    )
 
-    # store path 
-    pod_json_path = sys.argv[1]
+    # tell parser to expect path positional argument
+    parser.add_argument('path')
+
+    # store the namespace object to access command line args
+    namespace = parser.parse_args()
+
+    # store path of json
+    pod_json_path = namespace.path
+
+    # get path of json file from command line arg
+    # if len(sys.argv) < 2:
+    #     print("Usage: python kubeguard.py <pods.json>")
+    #     sys.exit(1)
 
     # parse the json into dictionary
-    pods_data = input.load_pods(pod_json_path)
+    pods_data = loader.load_pods(pod_json_path)
 
     # validate that the pod has the correct fields
-    input.validate_pods_data(pods_data)
+    loader.validate_pods_data(pods_data)
 
     # obtain dictionary of pods and their statuses
     unhealthy_pods = analyzer.find_unhealthy_pods(pods_data)
